1dff3f5c7a7bf35a1de033cf97e13e711cbbcf79
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-08-26 13:07:33 +0300

    models

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Tasks(models.Model):
+class Task(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField(max_length=200)
     status=models.BooleanField(default=False,verbose_name='Completed')
--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include("todo_app.urls")),
+    path('',include("todo_app.urls")),
 ]
