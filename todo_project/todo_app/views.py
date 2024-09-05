6ffc71880fe01c1770d776b26afc2a2a7de123a3
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-09-05 16:15:07 +0300

    Model relationships and class based vies

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,15 +1,18 @@
+from typing import Any
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Task
 from .forms import TaskCreateForm
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.contrib.auth.decorators import login_required
-
-
-
-
+from django.views.generic import View,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
 # Create your views here.
+class TaskListView(View):
+   template_name='todo_app/list.html' 
+   
 
-def task_list(request):
+   def get(self,request):
     tasks= Task.objects.all()
     paginator=Paginator(tasks,3,orphans=4,allow_empty_first_page=True)
 
@@ -23,46 +26,91 @@ def task_list(request):
         paginated_tasks=paginated_tasks.page(paginator.num_pages)
     
     title='List'
-    return render(request,'todo_app/list.html',{'tasks':paginated_tasks,'title':title})
+    context={'tasks':paginated_tasks,'title':title}
+    return render(request,self.template_name,context)
 
-   
-def task_detail(request,id):
-    task=get_object_or_404(Task,id=id)
-    #print(task)
-    title='Detail'
-    return render(request,'todo_app/detail.html',{'task':task,'title':title})
+class TaskDetailView(DetailView):
+    model=Task
+    template_name='todo_app/detail.html'
+    context_object_name='task'
 
-@login_required
-def task_create(request):
-    if request.method == 'POST':
-        form=TaskCreateForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('list')
-    else:
-            form=TaskCreateForm()
-            title='Create'
-    return render(request,'todo_app/create.html',{'form':form,'title':title})
-@login_required
-def task_update(request,id):
-     task=get_object_or_404(Task,id=id)
-     if request.method=='POST':
-          form=TaskCreateForm(request.POST or None,instance=task)
-          if form.is_valid():
-           form.save()
-           return redirect('list')
-     else:
-          form=TaskCreateForm(instance=task)
-          title='Update'
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['title']='Task'
+        return context
+    # task=get_object_or_404(Task,id=id)
+    # #print(task)
+    # title='Detail'
+    # return render(request,'todo_app/detail.html',{'task':task,'title':title})
 
-     return render(request,'todo_app/update.html',{'form':form,'title':title,'task':task})
+# @login_required
+class TaskCreateView(LoginRequiredMixin,CreateView):
+    model=Task
+    form=TaskCreateForm
+    fields=['title','description','status','due_date','author']
+    template_name='todo_app/create.html'
+    success_url='/'
+
+    def get_context_data(self, **kwargs): 
+        context= super().get_context_data(**kwargs)
+        context['title']='Create '
+        return context
+
+# def task_create(request):
+#     if request.method == 'POST':
+#         form=TaskCreateForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('list')
+#     else:
+#             form=TaskCreateForm()
+#             title='Create'
+#     return render(request,'todo_app/create.html',{'form':form,'title':title})
+# @login_required
+@method_decorator(login_required,name='dispatch')
+class TaskUpdateView(LoginRequiredMixin,UpdateView):
+    model=Task
+    form=TaskCreateForm
+    fields=['title','description','status','due_date','author']
+    template_name='todo_app/update.html'
+    success_url='/'
+
+    def get_context_data(self, **kwargs): 
+        context= super().get_context_data(**kwargs)
+        context['title']='Update '
+        return context
+
+# def task_update(request,id):
+#      task=get_object_or_404(Task,id=id)
+#      if request.method=='POST':
+#           form=TaskCreateForm(request.POST or None,instance=task)
+#           if form.is_valid():
+#            form.save()
+#            return redirect('list')
+#      else:
+#           form=TaskCreateForm(instance=task)
+#           title='Update'
+
+#      return render(request,'todo_app/update.html',{'form':form,'title':title,'task':task})
  
-@login_required
-def task_delete(request,id):
-    task=get_object_or_404(Task, id=id)
-    if request.method=='POST':
-        task.delete()
-        return redirect('list')
-    title='Delete'
+# @login_required
+# def task_delete(request,id):
+#     task=get_object_or_404(Task, id=id)
+#     if request.method=='POST':
+#         task.delete()
+#         return redirect('list')
+#     title='Delete'
     
-    return render(request,'todo_app/delete.html',{'task':task,'title':title,'task':task})
+#     return render(request,'todo_app/delete.html',{'task':task,'title':title,'task':task})
+@method_decorator(login_required,name='dispatch')
+class TaskDeleteView(LoginRequiredMixin,DeleteView):
+    model=Task
+    form=TaskCreateForm
+    fields=['title','description','status','due_date','author']
+    template_name='todo_app/delete.html'
+    success_url='/'
+
+    def get_context_data(self, **kwargs): 
+        context= super().get_context_data(**kwargs)
+        context['title']='Delete '
+        return context
